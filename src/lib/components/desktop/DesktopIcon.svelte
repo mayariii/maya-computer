6bebfb2a42da383593e8caa5e5f50c3c4ba9aabe
Maya Avendano <mayari.avendano@gmail.com>
2025-01-12 22:09:53 +0000

    change forgotten component from svelte 4 to 5

--- src/lib/components/desktop/DesktopIcon.svelte
+++ src/lib/components/desktop/DesktopIcon.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	export let icon: string;
-	export let label: string;
-	export let onClick: () => void;
+	const { icon, label, onClick } = $props();
 </script>
 
-<button
-	on:click={onClick}
-	class="flex w-[75px] flex-col items-center rounded p-3 focus:bg-white/20"
->
+<button onclick={onClick} class="flex w-[75px] flex-col items-center rounded p-3 focus:bg-white/20">
 	<img src={icon} alt={label} class="mb-1 h-8 w-8" />
 	<span class="text-shadow break-words text-center font-departure text-xs text-white">
 		{label}
